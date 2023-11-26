d4f4814f54715729b74cb2b9091dc2697c51c7bb
Vikar Maulana Arrisyad <vikar.maulana.arrisyad@gmail.com>
2023-11-26 08:57:06 +0700

    add ui login

--- lib/module/login/view/login_view.dart
+++ lib/module/login/view/login_view.dart
@@ -1,4 +1,5 @@
-
+import 'package:absensi_rfid_flutter/components/background.dart';
+import 'package:absensi_rfid_flutter/core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../bloc/login_bloc.dart';
@@ -51,33 +52,105 @@ class _LoginViewState extends State<LoginView> {
     );
   }
 
-  Widget buildView( 
-      BuildContext context,
-      LoginBloc bloc,
-      LoginState state,
-    ) {
+  Widget buildView(
+    BuildContext context,
+    LoginBloc bloc,
+    LoginState state,
+  ) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Text(
-            'Counter: ${state.counter}',
-            style: const TextStyle(fontSize: 24),
-          ),
-          IconButton(
-            onPressed: () => bloc.add(LoginIncrementEvent()),
-            icon: const Icon(
-              Icons.add,
-              size: 24.0,
+      body: Background(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: Text(
+                "LOGIN",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF2661FA),
+                    fontSize: 36),
+                textAlign: TextAlign.left,
+              ),
             ),
-          ),
-        ],
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: 40),
+              child: TextField(
+                decoration: InputDecoration(labelText: "Username"),
+              ),
+            ),
+            SizedBox(height: 2.0),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: 40),
+              child: TextField(
+                decoration: InputDecoration(labelText: "Password"),
+                obscureText: true,
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerRight,
+              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: Text(
+                "Forgot your password?",
+                style: TextStyle(fontSize: 12, color: Color(0XFF2661FA)),
+              ),
+            ),
+            SizedBox(height: 5.0),
+            Container(
+              alignment: Alignment.centerRight,
+              margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueGrey,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(80.0)),
+                    padding: const EdgeInsets.all(0)),
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 50.0,
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  decoration: new BoxDecoration(
+                      borderRadius: BorderRadius.circular(80.0),
+                      gradient: new LinearGradient(colors: [
+                        Color.fromARGB(255, 255, 136, 34),
+                        Color.fromARGB(255, 255, 177, 41)
+                      ])),
+                  child: Text(
+                    "LOGIN",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  padding: const EdgeInsets.all(0),
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerRight,
+              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: GestureDetector(
+                onTap: () => {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => RegisterView()))
+                },
+                child: Text(
+                  "Don't Have an Account? Sign up",
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF2661FA)),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
-}    
-    
+}
